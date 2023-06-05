f7657d729c2646a18e1ffa0628e57b5dcf73d0e4
Jess Rose <jrosehow@gmail.com>
2023-06-04 20:37:30 -0400

    finish main functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss';
+import data from './assets/data.json';
+import { DateTime } from 'luxon';
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const weekTotal = data.reduce((x, a) => x + a.amount, 0);
+
+const Bar = (props: any) => {
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  const barSize = props.amount / 7;
+  const isCurrentDay = () => {
+    const currentDay = DateTime.now().weekdayShort.toLowerCase();
+    if (currentDay === props.day) {
+      return true;
+    }
+    return false;
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div
+        className='price-label'
+        style={{ visibility: `${isMouseOver ? 'initial' : 'hidden'}` }}
+      >
+        ${props.amount}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div
+        className={isCurrentDay() ? 'bar current' : 'bar'}
+        style={{ height: `${barSize}em` }}
+        onMouseOver={() => setIsMouseOver(true)}
+        onMouseLeave={() => setIsMouseOver(false)}
+      ></div>
+      <span className='day-label'>{props.day}</span>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className='main-container'>
+      <h2>Spending - Last 7 Days</h2>
+      <div className='graph-container'>
+        {data.map((x) => (
+          <Bar key={x.day} day={x.day} amount={x.amount} />
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
